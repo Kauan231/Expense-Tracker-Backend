0f24af8c0fdce01c988f042ba133ac69071073ae
Kauan231 <kauanramos03@outlook.com>
2026-01-25 19:39:35 -0300

    Adding notification

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const swaggerFile = require('./swagger');
 const routes = require('./routes');
 const path = require('path');
 const db = require('./models');
-
 const app = express();
-
+const { InvoiceTrackerController } = require('./controller');
 /* Middlewares */
 if(process.env.DEV) {
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -18,6 +17,62 @@ app.use(cors());
 /* Uploads */
 const UPLOADS_DIR = process.env.UPLOADS_DIR || path.join(__dirname, 'uploads');
 app.use('/uploads', express.static(UPLOADS_DIR));
+
+async function getPendingInvoices () {
+  let invoiceTrackerController = new InvoiceTrackerController();
+  let allInvoiceTrackers;
+  try { allInvoiceTrackers = await invoiceTrackerController.ReadAllWithSkipLimit(0, 1000);
+  } catch(err) { return console.error('Failed to get pending invoices', err); }
+
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth() + 1;
+  const monthName = new Date(year, month - 1).toLocaleString("pt-Br", { month: "long" }).toLocaleUpperCase();
+  const today = new Date().getDate();
+
+  let pendingMessages = [];
+  let expiredMessages = [];
+  let allInvoiceTrackerIds = allInvoiceTrackers.map(tracker => tracker.id);
+  for (let id of allInvoiceTrackerIds) {
+    let tracker = await invoiceTrackerController.ReadById(id, year, month);
+
+    const invoices = tracker.Invoices ?? [];
+    const dueDate = tracker.dueDate;
+
+    const invoiceForMonth = invoices.find(inv => {
+      const invDate = new Date(inv.date);
+      return invDate.getFullYear() === year && (invDate.getMonth() + 1) === month;
+    });
+
+    if (invoiceForMonth?.status != 1) {
+      if (today > dueDate) {
+        pendingMessages.push(
+          `❗ "${tracker.name}" → Nenhum comprovante enviado para o período ${monthName} ${year}. A fatura venceu em ${dueDate}.`
+        );
+      } else {
+        expiredMessages.push(
+          `⚠️ "${tracker.name}" → Nenhum comprovante enviado para o período ${monthName} ${year}. A data de vencimento é ${dueDate}.`
+        );
+      }
+    }
+  }
+
+  for(let message of pendingMessages) {
+    notify("Fatura pendente", message)
+  }
+
+  for(let message of expiredMessages) {
+    notify("Fatura vencida", message)
+  }
+}
+
+function notify(title, body) {
+  if (process.send) {
+    process.send({
+      type: "notify",
+      payload: { title, body }
+    });
+  }
+}
 
 async function bootstrap() {
   try {
@@ -35,6 +90,8 @@ async function bootstrap() {
       console.log(`LAN: http://${IP}:${PORT}`);
       console.log(`Docs: http://${IP}:${PORT}/docs`);
     });
+
+    await getPendingInvoices();
   } catch (err) {
     console.error('Bootstrap failed:', err);
     process.exit(1);
