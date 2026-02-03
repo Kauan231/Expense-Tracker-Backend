68bd38718ad995b6bfc77b4807775d629a8589b1
Kauan231 <kauanramos03@outlook.com>
2026-02-03 19:09:25 -0300

    fix db location

--- models/index.js
+++ models/index.js
@@ -1,37 +1,46 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const allConfigs = require(path.join(__dirname, "../config/config.json"));
+const config = allConfigs[env] || allConfigs["development"];
+
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const storagePath =
+  process.env.SQLITE_PATH ||
+  path.join(process.cwd(), "database.sqlite3");
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
+console.log("📌 Sequelize ENV =", env);
+console.log("📌 SQLite Storage Path =", storagePath);
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: storagePath,
+  logging: config.logging || false,
+});
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-3) === ".js" &&
+      file.indexOf(".test.js") === -1
     );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
